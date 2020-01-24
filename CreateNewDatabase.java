70c8863ce558fecf26d60a8e9995fc1f467c3551
grdirkso <36120935+grdirkso@users.noreply.github.com>
2020-01-23 19:49:46 -0600

    writing to a fixed length file

--- CreateNewDatabase.java
+++ CreateNewDatabase.java
@@ -3,11 +3,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileWriter;
 
 public class CreateNewDatabase {
 
     public String fileName;
-    
+
+
     CreateNewDatabase() {
         // Ask user to enter a file
         System.out.println("Please enter name of your .csv file: ");
@@ -29,17 +33,29 @@ public class CreateNewDatabase {
         try {
 
             br = new BufferedReader(new FileReader(this.fileName));
+            FileWriter fileWriter = new FileWriter("Fortune_500_HQ.data");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
             while ((line = br.readLine()) != null) {
 
                 // use comma as separator
                 String[] country = line.split(cvsSplitBy);
 
-                for (int i = 0; i < 6; i++) {
-                    System.out.print(country[i] + " ");
-                }
-
-                System.out.println();
+                printWriter.printf("%-4s", country[0]);
+                printWriter.printf("%-40s", country[1]);
+                printWriter.printf("%-20s", country[2]);
+                printWriter.printf("%-6s", country[3]);
+                printWriter.printf("%-6s", country[4]);
+                printWriter.printf("%-10s", country[5]);
+                printWriter.printf("\n");
+                System.out.printf("%-4s", country[0]);
+                System.out.printf("%-40s", country[1]);
+                System.out.printf("%-20s", country[2]);
+                System.out.printf("%-6s", country[3]);
+                System.out.printf("%-6s", country[4]);
+                System.out.printf("%-10s", country[5]);
+                System.out.printf("\n");
             }
+            printWriter.close();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
